bf83801facaf8456ca114409253e8e18379e8f58
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-08-02 11:31:32 +0100

    Dynamically filtering a SwiftUI List

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -27,11 +27,30 @@ struct ProspectsView: View {
         }
     }
     
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
     
     var body: some View {
         NavigationView {
-        Text("People: \(prospects.people.count)")
-            .navigationBarTitle(title)
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }            .navigationBarTitle(title)
             .navigationBarItems(trailing: Button(action: {
                 let prospect = Prospect()
                 prospect.name = "Paul Hudson"
